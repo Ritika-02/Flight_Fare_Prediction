196c604e223b2714ee0660e355e889b9cc615710
Ritika-02 <ritikabhandari049@gmail.com>
2024-01-26 21:46:51 +0530

    form updated

--- app.py
+++ app.py
@@ -20,17 +20,17 @@ def predict_datapoint():
         data = CustomData(
 
             Airline= request.form.get('Airline'),
-            Source= request.form.get('Source'),
-            Destination= request.form.get('Destination'),
-            Total_Stops= request.form.get('Total_Stops'),
             Journey_Day= int(request.form.get('Journey_Day')),
             Journey_Month= int(request.form.get('Journey_Month')),
+            Source= request.form.get('Source'),
+            Destination= request.form.get('Destination'),
             Departure_Hour= int(request.form.get('Departure_Hour')),
             Departure_Min= int(request.form.get('Departure_Min')),
             Arrival_Hour= int(request.form.get('Arrival_Hour')),
             Arrival_Min= int(request.form.get('Arrival_Min')),
             Duration_Hour= int(request.form.get('Duration_Hour')),
-            Duration_Min= int(request.form.get('Duration_Min'))
+            Duration_Min= int(request.form.get('Duration_Min')),
+            Total_Stops= request.form.get('Total_Stops')
         )
 
         final_data = data.get_data_as_dataframe()
